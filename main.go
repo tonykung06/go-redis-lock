2d4acfcc2f386989c9f33606ebe6de3d45e96f32
tonykung <tonykung06@gmail.com>
2017-03-14 23:49:42 +0800

    add testing case

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import "github.com/garyburd/redigo/redis"
+import "time"
+import "flag"
+import "sync"
+import "fmt"
+import "runtime"
+import "encoding/json"
+
+
+type MyStruct struct {
+  Values []int
+}
+
+func newPool(addr string) *redis.Pool {
+  return &redis.Pool{
+    MaxIdle: 3,
+    IdleTimeout: 240 * time.Second,
+    Dial: func () (redis.Conn, error) { return redis.Dial("tcp", addr, redis.DialDatabase(1)) },
+  }
+}
+
+var (
+  pool *redis.Pool
+  redisServer = flag.String("redisServer", "localhost:6379", "")
+)
+
+func main() {
+  runtime.GOMAXPROCS(4)
+  
+  fmt.Println("redis go starts")
+  flag.Parse()
+  pool = newPool(*redisServer)
+  conn := pool.Get()
+  _, err := conn.Do("DEL", "testing")
+  conn.Close()
+  if err != nil {
+    fmt.Println("Failed cleanup testing beforehand")
+    return
+  }
+
+  var wg sync.WaitGroup
+  for i := 1; i <= 1000; i++ {
+    wg.Add(1)
+    go func(count int){
+      defer wg.Done()
+      conn := pool.Get()
+      defer conn.Close()
+
+      val, err := redis.String(conn.Do("GET", "testing"))
+      if err != nil || val == "" {
+        myStructVal := MyStruct{
+          Values: []int{count},     
+        }
+        testing, err := json.Marshal(myStructVal)
+        if err != nil {
+          fmt.Println("json marshal error:", err)
+          return
+        }
+        _, err = conn.Do("SET", "testing", string(testing))
+        if err != nil {
+          fmt.Println("Failed to write: ", err)
+        }
+        return
+      }
+
+      unmarshaledVal := &MyStruct{}
+      err = json.Unmarshal([]byte(val), unmarshaledVal)
+      if err != nil {
+        fmt.Println("json unmarshal error:", err)
+        return
+      }
+      unmarshaledVal.Values = append(unmarshaledVal.Values, count)
+      saveVal, err := json.Marshal(unmarshaledVal)
+      if err != nil {
+        fmt.Println("json marshal error:", err)
+        return
+      }
+      _, err = conn.Do("SET", "testing", saveVal)
+      if err != nil {
+        fmt.Println("Failed to write: ", err)
+      }
+    }(i)
+  }
+  wg.Wait()
+}
